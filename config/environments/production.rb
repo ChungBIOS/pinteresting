eb0309261d2cd27310d9e1b64b4bb5b49bf7d5ce
Andy Chung <andy@hatchstud.io>
2014-01-27 17:56:01 +0000

    added devise and configured

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ FirstRailsApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Devise config
+  config.action_mailer.default_url_options = { :host => 'http://andy-omr.herokuapp.com' }
 end
